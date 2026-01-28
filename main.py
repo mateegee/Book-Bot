3b335b5458a2cc10b8f03935c7ae463d303d3406
mateegee <matt.grant11@hotmail.com>
2026-01-28 14:42:02 +0000

    added file reading and word count functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        book_contents = f.read()
+        return book_contents
+
+def count_words(book):
+    words = book.split()
+    return len(words)
+
+def main():
+    frankenstein = get_book_text("books/frankenstein.txt")
+    word_count = count_words(frankenstein)
+    print(f"Found {word_count} total words")
+
+main()
