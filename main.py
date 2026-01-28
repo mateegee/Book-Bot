c84cce3ff4fe366550ff7c5ad8894f529cdfcc12
mateegee <matt.grant11@hotmail.com>
2026-01-28 14:44:36 +0000

    created stats.py and moved text-analysis functions to stats from main

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        book_contents = f.read()
-        return book_contents
-
-def count_words(book):
-    words = book.split()
-    return len(words)
+from stats import get_book_text, count_words
 
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
