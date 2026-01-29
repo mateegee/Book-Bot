f0a1617800d3c4e895dfb96fa75f503e46cc9f2e
mateegee <matt.grant11@hotmail.com>
2026-01-29 16:17:05 +0000

    optional extra added to stats: word quantity count. also added waterloo.txt to books

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from stats import get_book_text, count_words, character_count, character_sorting, sort_on, book_report
-from pprint import pprint
+from stats import book_report
 import sys
 
 def main():
     book_report()
-    
+
 print("Usage: python3 main.py <path_to_book>")
 main()
--- stats.py
+++ stats.py
@@ -19,8 +19,29 @@ def character_count(book):
             characters[c] = 1
     return characters
 
-def sort_on(items):
+def sort_on_chars(items):
     return items["num"]
+
+def sort_on_words(items):
+    for key, value in items.items():
+        return items[key]
+
+def word_quantity(book):
+    words = book.lower().split()
+    word_dict = {}
+    clean_words = []
+    for word in words:
+        word = ''.join(char for char in word if char.isalpha())  # remove punctuation
+        if word:  # skip empty strings
+            clean_words.append(word.lower())  # optional lowercase
+    print(clean_words)
+
+    for clean_word in clean_words:
+        if clean_word not in word_dict:
+            word_dict[clean_word] = 1
+        else:
+            word_dict[clean_word] += 1
+    return word_dict
 
 def character_sorting(character_count_dict):
     sorted_list = []
@@ -30,15 +51,26 @@ def character_sorting(character_count_dict):
         else:
             sorted_list.append({"char": key, "num": value})
     
-    sorted_list.sort(reverse=True, key=sort_on)
+    sorted_list.sort(reverse=True, key=sort_on_chars)
     return sorted_list
+
+def word_sorting(words_dict):
+    words_list = []
+    for key, value in words_dict.items():
+        words_list.append({key: value})
+    
+    words_list.sort(reverse=True, key=sort_on_words)
+    return words_list
 
 def book_report():
     book_location = sys.argv[1]
-    frankenstein = get_book_text(book_location)
-    word_count = count_words(frankenstein)
-    characters = character_count(frankenstein)
+    book = get_book_text(book_location)
+    word_count = count_words(book)
+    characters = character_count(book)
+    word_num = word_quantity(book)
+    sorted_words = word_sorting(word_num)
     sorted_characters = character_sorting(characters)
+
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_location}...")
@@ -47,6 +79,10 @@ def book_report():
     print("--------- Character Count -------")
     for char_dict in sorted_characters:
         print(f"{char_dict["char"]}: {char_dict["num"]}")
+    print("---------- Word Count -----------")
+    for word_dict in sorted_words:
+        for key, value in word_dict.items():
+            print(f"{key}: {value}")
     print("============= END ===============")
 
     
